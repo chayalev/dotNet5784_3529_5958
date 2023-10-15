e687e65439f971d4725969a819b0c62e38cd23a2
chayale <chayalegut@gmail.com>
2023-10-15 15:47:30 +0300

    initial code + Welcome functions

---
+++ Stage0/Program3529.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ex1
+{
+    partial class Program
+    {
+         static void Main(string[] args)
+        {
+            Welcome3529();
+            Welcome5958();
+            Console.ReadKey();
+        }
+        static partial void Welcome5958();
+        private static void Welcome3529()
+        {
+            Console.Write("Enter your name: ");
+            string nameUser = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", nameUser);
+        }
+    }
+}
