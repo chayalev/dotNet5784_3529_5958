d41beadf8b207c67657d81677e299bd78a05c2ee
Avital <avutals5050@gmail.com>
2023-11-13 21:35:27 +0200

    record entities dependency + engineer

---
+++ DalFacade/DO/Dependency.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DO;
+/// <summary>
+///  Dependency Entity represents a dependence with all its props
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="DependentTask"></param>
+/// <param name="DependsOnTask"></param>
+public record Dependency
+(
+    int Id,
+    int DependentTask,
+    int DependsOnTask
+);
--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,9 +1,19 @@
 ﻿
 
 namespace DO;
-
+/// <summary>
+/// Engineer Entity represents an engineer with all its props
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Name"></param>
+/// <param name="Level"></param>
+/// <param name="Email"></param>
+/// <param name="Cost"></param>
 public record Engineer
-{
-    
-
-}
+(
+    int Id,
+    string Name,
+    publicEngineerExperience Level,
+    string? Email = null,
+    double? Cost = null
+);
