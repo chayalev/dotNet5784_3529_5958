8dbcf45f04a513a90c66379bf92a4cafcc6d14d0
Avital <avutals5050@gmail.com>
2024-01-04 13:17:52 +0200

    begin stage 4

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -2,8 +2,11 @@
 using DalApi;
 using System;
 
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    public static IDal Instance { get; } = new DalList();
+    private DalList() { }
+
     public IDependency Dependency => new DependencyImplementation();
 
     public IEngineer Engineer => new EngineerImplementation();
--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,11 +1,15 @@
 ﻿using DalApi;
+using System.Diagnostics;
 using System.Linq.Expressions;
 
 namespace Dal;
 
 //stage 3
-sealed public class DalXml : IDal
+sealed internal class DalXml : IDal
 {
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
+
     public IDependency Dependency => new DependencyImplementation();
 
     public IEngineer Engineer => new EngineerImplementation();
