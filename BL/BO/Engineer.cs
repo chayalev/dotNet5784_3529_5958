711401e1a2727db1f91abbdfc9ee5381d9a47bd0
chayale <chayalegut@gmail.com>
2024-01-06 21:26:43 +0200

    stage4 begin ex6

---
+++ BL/BO/Engineer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Engineer
+    {
+        public int Id {  get; init; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public enum EngineerExperience { Novice, AdvancedBeginner, Competent, Proficient, Expert };
+        public double? Cost { get; set; }
+        public TaskInEngineer? Task {  get; set; }
+    }
+}
---
+++ BL/BO/TaskInEngineer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class TaskInEngineer
+    {
+        public int Id {  get; init; }
+        public string? Alias { get; set; } 
+    }
+}
